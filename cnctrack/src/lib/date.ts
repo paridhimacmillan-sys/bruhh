1f087e19d547c30fb3ec07ad18153de8a8e79db0
paridhimacmillan-sys <paridhi.macmillan@gmail.com>
2026-05-30 10:47:28 +0530

    Delete cnctrack directory

--- cnctrack/src/lib/date.ts
+++
@@ -1,8 +0,0 @@
-export function getTodayISOLocal(): string {
-  const now = new Date();
-  const yyyy = now.getFullYear();
-  const mm = String(now.getMonth() + 1).padStart(2, '0');
-  const dd = String(now.getDate()).padStart(2, '0');
-  return `${yyyy}-${mm}-${dd}`;
-}
-
